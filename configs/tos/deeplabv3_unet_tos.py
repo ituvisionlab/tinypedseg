9eaff2a74e6c0945eaf697a8e65d6ae453696773
Elvin Abdinli <43825683+abdinli18@users.noreply.github.com>
2025-02-01 22:00:23 +0100

    Update data_root path

--- configs/tos/deeplabv3_unet_tos.py
+++ configs/tos/deeplabv3_unet_tos.py
@@ -51,7 +51,7 @@ model = dict(
 
 # dataset settings
 dataset_type = 'TinyObjectSegmentationDataset'
-data_root = './tinypedseg'  # Update with your data root
+data_root = './'  # Update with your data root
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 
